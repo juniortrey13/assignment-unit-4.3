b4a9fda1715b97a2b0dfafb56b54340c26e17ade
Junior Avalos <junior.trey.avalos13@gmail.com>
2024-10-03 20:51:31 -0500

    Anwsered the required parts of the assignment

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,8 +1,19 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
-
-
+let basket = []; // created our empty basket
+function addItem ( item ) {
+    basket.push ( item );
+    return true;
+}
+function listItems(){
+    for ( let i = 0; i < basket.length; i++ ) {
+        console.log( basket[i] );
+    }
+}
+function empty() {
+    basket.length = 0;
+}
 
 
 
