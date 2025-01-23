55737cb1d2ad4e6dbcdb5172a84e515425dd64ce
Junior Avalos <junior.trey.avalos13@gmail.com>
2025-01-23 12:15:55 -0600

    added the isFull function and updated my addItem function

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -8,7 +8,11 @@ console.log( 'Basket is:', basket );
 
 // created a function called addItem
 function addItem ( item ) {  // Takes an input parameter
+    if (isFull()){
+        return false;
+    }
     basket.push ( item ); // Pushes my new item into the basket array.
+    console.log( 'Adding item into your basket!');
     return true; // Returns true to indicate succesfull addition.
 }
 // In the console add an item by typing addItem ( '[item]' ) and press ented
@@ -25,8 +29,18 @@ function empty() {
     console.log('Emptying basket...');
 }
 
+// 1.
 const maxItems = 5; // we are using const because the value is not expected to change
-
+// 2.
+function isFull() {
+    if ( basket.length < maxItems ) {
+        console.log ('Your basket is not full yet!')
+        return false;
+    } else {
+        console.log ('Oops! Your basket is full!'); 
+        return true;
+    }
+}
 
 
 
